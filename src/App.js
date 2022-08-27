bf0fdedba231a55b4c1eb177868085fdefba0326
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-08-27 14:00:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App(){
   return(
     <div>
       <h1 className="bg-red-500 text-7xl w-[500px] rounded-[15px]">Hello</h1>
-      <p>You are welcome here here</p>
+      <p>Hello</p>
     </div>
   );
 }
